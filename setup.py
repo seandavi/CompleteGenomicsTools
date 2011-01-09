6f2287e99fef8b32c47a7dcd16ab58d99a46b487
Sean Richard Davis <sedavis@helix.nih.gov>
2011-01-09 13:11:06 -0500

    Added python distutils structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='CompleteGenomicsTools',
+      version=version,
+      description="Tools for manipulating and visualizing data from Complete Genomics",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='genomics biology bioinformatics sequencing',
+      author='Sean Davis',
+      author_email='seandavi@gmail.com',
+      url='https://github.com/seandavi/CompleteGenomicsTools',
+      license='GPL-2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
