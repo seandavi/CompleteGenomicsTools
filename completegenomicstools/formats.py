94c0c476c374571b00eb0c8d7d2d8b335694510c
Sean Richard Davis <sedavis@helix.nih.gov>
2011-01-25 22:18:19 -0500

    Added parser for SuperlocusVariant.tsv files

--- completegenomicstools/formats.py
+++ completegenomicstools/formats.py
@@ -67,5 +67,98 @@ class DepthOfCoverageFile:
                      float(tmp[4]),
                      float(tmp[5])]))
 
+
+class SimpleGenotype(tuple):
+    """
+    Store a genotype as a simple pair of alleles.
+    """
+    def __init__(self,alleles):
+        if(len(alleles)!=2):
+            raise(Exception("need two alleles"))
+        super(SimpleGenotype,self).__init__(alleles)
+
+    def __str__(self):
+        return("%s;%s" % (self))
+
+
+class SuperlocusVariantRecord:
+    """
+    Encapsulates a single row of a SuperlocusVariantRecord
+
+    The attributes look like this:
     
+    def __init__(self,row):
+        self.superLocusId=int(row[0])
+        self.chromosome=row[1]
+        self.start=int(row[2])
+        self.end=int(row[3])
+        self.alleleClasses=row[4].split(';')
+        self.refAllele=row[5]
+        self.tumorAlleles=SimpleGenotype(row[6].split(";"))
+        self.normalAlleles=SimpleGenotype(row[7].split(";"))
+    """
+    def __init__(self,row):
+        self.superLocusId=int(row[0])
+        self.chromosome=row[1]
+        self.start=int(row[2])
+        self.end=int(row[3])
+        self.alleleClasses=row[4].split(';')
+        self.refAllele=row[5]
+        self.tumorAlleles=SimpleGenotype(row[6].split(";"))
+        self.normalAlleles=SimpleGenotype(row[7].split(";"))
         
+    def __str__(self):
+        return("%d\t%s\t%d\t%d\t%s;%s\t%s\t%s\t%s" % (self.superLocusId,
+                                                      self.chromosome,
+                                                      self.start,
+                                                      self.end,
+                                                      self.alleleClasses[0],
+                                                      self.alleleClasses[1],
+                                                      self.refAllele,
+                                                      str(self.tumorAlleles),
+                                                      str(self.normalAlleles)))
+    
+class SuperlocusVariantFile:
+    """
+    This class encapsulates the so-called SuperlocusOutput.tsv file generated
+    by running cgatools calldiff.  An example looks like this:
+
+    SuperlocusId    Chromosome      Begin   End     Classification  Reference       AllelesA        AllelesB
+    1       chr1    41980   41981   alt-identical;alt-identical     A       G;G     G;G
+    2       chr1    55925   55926   alt-consistent;alt-consistent   T       ?;?     C;C
+    3       chr1    57951   57952   alt-identical;alt-identical     A       C;C     C;C
+    4       chr1    58210   58211   alt-identical;alt-identical     A       G;G     G;G
+    5       chr1    59039   59040   ref-identical;alt-consistent    T       T;N     T;C
+    6       chr1    61986   61989   ref-consistent;alt-consistent   AAG     ?;?     AAG;GAC
+    7       chr1    68893   68896   ref-consistent;alt-consistent   TAG     ?;TAA   ?;?
+    8       chr1    69452   69453   ref-consistent;alt-consistent   G       ?;?     G;A
+    9       chr1    69510   69511   alt-identical;alt-identical     A       G;G     G;G
+    10      chr1    69551   69552   ref-consistent;alt-consistent   G       G;C     ?;?
+    11      chr1    82733   82734   alt-identical;ref-consistent    T       C;N     C;T
+    12      chr1    87189   87190   ref-consistent;alt-consistent   G       G;A     ?;?
+    107     chr1    737909  737910  ref-identical;onlyA     G       G;A     G;G
+    350     chr1    909308  909309  alt-identical;onlyB     T       C;T     C;C
+    567     chr1    997442  997468  ref-identical;onlyA     CCTTGTCCCCGTTCCCTCCGTCCCTC      CCTTGTCCCCGTTCCCTCCGTCCCTC;     CCTTGTCCCCGTTCCCTCCGTCCCTC;?CCCGTTCCCTCCGTCCCTC
+    923     chr1    1453631 1453633 ref-identical;onlyA     CA      CA;     CA;CA
+    1072    chr1    1584626 1584632 ref-consistent;onlyB    CACCCG  CA?;CA? CACCCG;TGCCCA
+    1101    chr1    1594200 1594200 alt-identical;onlyA             T;T     T;
+    1102    chr1    1594209 1594210 ref-identical;onlyB     C       C;C     C;T
+    1103    chr1    1594220 1594221 ref-identical;onlyB     C       C;C     C;A
+
+
+    """
+
+    def __init__(self,fname):
+        self.fh=csv.reader(open(fname,'r'),delimiter="\t")
+        self.colnames=self.fh.next()
+
+    def _iter(self):
+        for i in self.fh:
+            yield i
+
+    def __iter__(self):
+        return(self)
+
+    def next(self):
+        row=self._iter().next()
+        return(SuperlocusVariantRecord(row))
